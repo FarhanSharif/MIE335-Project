b81519bba996c04b8bcb651c9107004cca5aa3e1
Farhan Sharif <farhanisharif@gmail.com>
2018-03-23 13:08:14 -0400

    Get M and H
    Based on train matrix, getMH(...) splits data into M and H matrices
    
    ** Issue of computation time because matrix size keeps changing; MATLAB suggests "Preallocation" **

---
+++ getMH.m
@@ -0,0 +1,33 @@
+function [M, H] = getMH(train)
+% [M, H] = getMH(train)
+% Splits train data into M and H matrices (splits Madison and Hamilton)
+
+% First index of train has labels 1 = Hamilton, 2 = Madison
+
+% We want to convert the labels to: 1 = Hamilton, -1 = Madison
+
+% For each data point in train...
+% if label = 1, put data in H, if label = 2, put data in M
+
+H = [];
+M = [];
+
+for ii = 1 : size(train, 1)
+    
+   currentRow = train(ii, :);
+   
+   if currentRow(1) == 1
+       % Put data from 2,: in H
+       H = [H; currentRow(2,:)];
+       
+   elseif currentRow(1) == 2
+       % Put data from 2,: in M
+       M = [M; currentRow(2,:)];
+       
+   end
+    
+end
+
+
+end
+
