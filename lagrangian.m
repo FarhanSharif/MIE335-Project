4b4e92ef121036e3523c6b5f3cd46e51ed471de4
albertloa <32650603+albertloa@users.noreply.github.com>
2018-03-29 13:44:52 -0400

    lagrangian.m update
    adjusted to include nonneg constraints w/o input in the A matrix

--- lagrangian.m
+++ lagrangian.m
@@ -1,11 +1,12 @@
 function [Aeq, beq] = lagrangian(G, g, A, b)
 
 % general QP -> KKT conditions
-% formulation from IPM.pdf, note that there "A" is A^T
+% formulation from IPM.pdf, note that their "A" is A^T
 
 % where the inputs are from the general QP formulation
 % min (1/2)(x^T)Gx + (g^T)x
-% s.t. (A^T)x >= b  (converted to equality cons later)
+% s.t. Ax >= b , x >= 0 (converted to equality cons later)
+% g, x, b are column vectors
 % Note: non negativity constraints (x >= 0) are placed in Ax >= b
 
 % *** outputs are matrices of equations, should all be set = 0 when used
@@ -19,14 +20,16 @@ function [Aeq, beq] = lagrangian(G, g, A, b)
 % C is lam*t = 0 for all constraints
 
 % returned: (leave out C cons, enforce manually)
-% Aeq = [L; F] = [G -A^T 0; -A 0 I]
-% beq = [-g; -b]
+% add (lam,t) for x >= 0 cons
+% Aeq = [L; F; x_nonneg_A] = [G -A^T 0; -A 0 I; I 0 -I]
+% beq = [-g; -b; 0]
 % x would be [x; lam; t]
 
-L_matrix = [G -A' zeros(size(G,1),size(A,1))]; 
-F_matrix = [-A zeros(size(A,1),size(A,1)) eye(size(A,1),size(A,1))];
+L_matrix = [G -A' -eye(size(G,1),size(G,1)) zeros(size(G,1),size(A,1)+size(G,1))]; 
+F_matrix = [-A zeros(size(A,1),size(A,1)+size(G,1)) eye(size(A,1),size(A,1)) zeros(size(A,1),size(G,1))];
+x_nonneg_A = [eye(size(G,1),size(G,1)) zeros(size(G,1),size(G,1)+size(A,1)) zeros(size(G,1),size(A,1)) -eye(size(G,1),size(G,1))];
 
-Aeq = [L_matrix; F_matrix];
-beq = [-g; -b];
+Aeq = [L_matrix; F_matrix; x_nonneg_A];
+beq = [-g; -b; zeros(size(G,1),1)];
 
 end
