cc7b7dd900f7b32eb540f06985618783f6b567a7
lingnath <32849838+lingnath@users.noreply.github.com>
2018-03-30 10:53:13 -0400

    Update simulatedannealing.m

--- simulatedannealing.m
+++ simulatedannealing.m
@@ -1,22 +1,24 @@
-function [x,y,z]=simulatedannealing()
-eps=0.001;
-
+function [x,y,z,time]=simulatedannealing()
+eps=0.00001;
+tic
 syms x y
 f=0.5*x^2+3*x*y+5*y^2-x-2*y;
 vec=[diff(f,x);diff(f,y)];
+vec=matlabFunction(vec);
+f=matlabFunction(f);
 %direction
 %how tf do we hardcode it so we dont have to manually type x=1,... in
 x=0;
 y=0;
 xold=[x;y];
-deltaf=eval(vec);
+deltaf=vec(x,y);
 %do a loop statement later
 scalardeltaf=sqrt(deltaf(1)^2+deltaf(2)^2);
 d=-1*deltaf./scalardeltaf;
 syms lam;
 x=x+lam*d(1);
 y=y+lam*d(2);
-theta=eval(f);
+theta=f(x,y);
 difftheta=diff(theta);
 eqn=difftheta==0;
 stepsize=solve(eqn,lam);
@@ -24,22 +26,22 @@ xnew=xold+stepsize*d;
 while(scalardeltaf>=eps)
     
     xold=xnew;
-    syms x y
-f=0.5*x^2+3*x*y+5*y^2-x-2*y;
-vec=[diff(f,x);diff(f,y)];
+    %syms x y
+%f=0.5*x^2+3*x*y+5*y^2-x-2*y;
+%vec=[diff(f,x);diff(f,y)];
 %direction
 %how tf do we hardcode it so we dont have to manually type x=1,... in
 x=xnew(1);
 y=xnew(2);
 xold=[x;y];
-deltaf=eval(vec);
+deltaf=vec(x,y);
 %do a loop statement later
 scalardeltaf=sqrt(deltaf(1)^2+deltaf(2)^2);
 d=-1*deltaf./scalardeltaf;
 syms lam;
 x=x+lam*d(1);
 y=y+lam*d(2);
-theta=eval(f);
+theta=f(x,y);
 difftheta=diff(theta);
 eqn=difftheta==0;
 stepsize=solve(eqn,lam);
@@ -47,5 +49,6 @@ xnew=xold+stepsize*d;
 end
 x=xnew(1);
 y=xnew(2);
-z=eval(f);
+z=f(x,y);
+time=toc;
 end
