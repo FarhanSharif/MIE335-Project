033cee99b47ab7017a023d39c141049888ec3862
Farhan Sharif <farhanisharif@gmail.com>
2018-03-29 20:04:42 -0400

    Started Revised Simplex to min a
    Currently working with simple examples, passing in matrices where a is already incorporated to test revised simplex algorithm.
    After more tests, will try to implement an algorithm to incorporate a from initial matrices.

---
+++ revised_simplex.m
@@ -0,0 +1,55 @@
+function x = revised_simplex(x, cBT, cN, B, N)
+
+num_basic = size(B, 2); % # of basic variables
+num_nonbasic = size(N, 2); % # of nonbasic variables
+
+% Optimality check
+r = cN - ((cBT * (B^-1))*N)';
+[value, enter_index] = min(r);
+
+% If negative value exists, then continue until no more negative values
+while value < 0
+    
+    % Make unity vector
+    e = zeros(num_nonbasic, 1);
+    e(enter_index) = 1;
+    
+    % Calculate d (needed to find min ratio)
+    d = [((-(B^-1)) * N(:, enter_index)); e];
+    
+    % Calculate ratios
+    ratios = (-x) ./ d;
+    
+    % To find min positive ratio, convert all nonpositive ratios to inf
+    ratios(ratios <= 0) = inf; 
+    
+    % Find min ratio (a) & corresponding index in basic matrix (exit_index)
+    [a, exit_index] = min(ratios);
+    
+    % Update x
+    x = x + (a * d);
+    
+    % Swap variables so that basic variables are at the top
+    temp = x(exit_index);
+    x(exit_index) = x(num_basic + enter_index);
+    x(num_basic + enter_index) = temp;
+    
+    % Swap exiting and entering variable columns in B and N matrices
+    temp = B(:, exit_index);
+    B(:, exit_index) = N(:, enter_index);
+    N(:, enter_index) = temp;
+    
+    % Swap exiting and entering variable positions in cB and cN matrices
+    temp = cBT(exit_index);
+    cBT(exit_index) = cN(enter_index);
+    cN(enter_index) = temp;
+    
+    % Optimality Test
+    r = cN - ((cBT * (B^-1))*N);
+    [value, enter_index] = min(r);
+    
+end
+
+
+end
+
