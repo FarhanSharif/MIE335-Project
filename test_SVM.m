9af1d5a4b333078d3a36f52a41d5b20164de08f6
Farhan Sharif <farhanisharif@gmail.com>
2018-04-01 14:57:54 -0400

    Testing quadprog vs simplex (no URS)
    - Simplex doesnt work
    - can see quadprog output side by side

--- test_SVM.m
+++ test_SVM.m
@@ -3,18 +3,17 @@ function [x_quadprog_vs_simplex, time_simplex] = test_SVM(mu)
 [train,tune,test,dataDim] = getFederalistData;
 [M, H] = getMH(train);
 
-[Q_simplex, c_simplex, A_simplex, b_simplex] = intialize_URS(M, H, mu);
+[Q, c, A, b, LB, UB] = intialize(M, H, mu);
 
 tic
-[Aeq, beq, x_dim] = lagrangian(Q_simplex, c_simplex, A_simplex, b_simplex);
-[x, x_positions, complementary_positions, basis_lookup, cB, cN, B, N] = initialize_BFS(Aeq, beq, x_dim);
-[x_simplex, lambda_simplex, mu_simplex, slack_simplex] = revised_simplex(x, x_positions, complementary_positions, basis_lookup, cB, cN, B, N, x_dim);
+[Aeq, beq, x_dim] = lagrangian(Q, c, A, b);
+% [x, x_positions, complementary_positions, basis_lookup, cB, cN, B, N] = initialize_BFS(Aeq, beq, x_dim);
+% [x_simplex, lambda_simplex, mu_simplex, slack_simplex] = revised_simplex(x, x_positions, complementary_positions, basis_lookup, cB, cN, B, N, x_dim);
+[tableau, vars_in_basis, complementary_positions, basis_lookup] = find_IBFS_regular_simplex(Aeq, beq, x_dim);
+[x_simplex, lambda_simplex, mu_simplex, slack_simplex] = simplex(tableau, vars_in_basis, complementary_positions, basis_lookup, x_dim);
 time_simplex = toc;
 
-
-[Q_quadprog, c_quadprog, A_quadprog, b_quadprog, LB_quadprog, UB_quadprog] = initialize(M, H, mu);
-
-[x_quadprog, fval, exitflag, output, lambda] = quadprog(Q_quadprog, c_quadprog, A_quadprog, b_quadprog, [], [], LB_quadprog, UB_quadprog);
+[x_quadprog, fval, exitflag, output, lambda] = quadprog(Q, c, A, b, [], [], LB, UB);
 
 % Put x_quadprog and x_simplex side by side for comparison
 x_quadprog_vs_simplex = [x_quadprog x_simplex];
